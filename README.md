f55dc159f8e4e86de36d3503e1879d4f48ca299c
Filip Kublin <fpkublin@gmail.com>
2020-05-05 14:11:27 +0200

    Initial commit for the project

---
+++ README.md
@@ -0,0 +1,2 @@
+Student project - service allowing for creation of Spotify playlists based on a seed created with user preference data.
+Exercise in React and Express.js.
---
+++ client/src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import SpotifyWebApi from 'spotify-web-api-js';
+const spotifyApi = new SpotifyWebApi();
+
+class App extends Component {
+  constructor(){
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: '' }
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying(){
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        this.setState({
+          nowPlaying: { 
+              name: response.item.name, 
+              albumArt: response.item.album.images[0].url
+            }
+        });
+        console.log('Checked');
+      })
+  }
+
+  generatePlaylist(){
+    var seed = { limit: 10, min_energy: 0.4, seed_artists: ['6mfK6Q2tzLMEchAr0e9Uzu', '4DYFVNKZ1uixa6SQTvzQwJ'], min_popularity: 50 };
+    spotifyApi.getRecommendations(seed).then((response) => {
+      console.log(response)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <a href='http://localhost:8888' > Login to Spotify </a>
+        <div>
+          Now Playing: { this.state.nowPlaying.name }
+        </div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} alt="Placeholder"/>
+        </div>
+        { this.state.loggedIn &&
+          <button onClick={() => this.getNowPlaying()}>
+            Check Now Playing
+          </button>
+        }
+
+        { this.state.loggedIn &&
+          <button onClick={() => this.generatePlaylist()}>
+            Generate a playlist
+          </button>
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
